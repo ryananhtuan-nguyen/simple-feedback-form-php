4971bbb3002b9e48208410a35e6fb12bf0bf098e
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-10-24 20:00:42 +1300

    name validation

--- index.php
+++ index.php
@@ -1,11 +1,50 @@
 <?php include 'inc/header.php'; ?>
+
+<?php
+$name = $email = $body = '';
+$nameErr = $emailErr = $bodyErr = '';
+
+
+//Form submit
+if (isset($_POST['submit'])) {
+  //Validate name
+  if (empty($_POST['name'])) {
+    $nameErr = 'Name is required';
+  } else {
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  }
+
+  //validate email
+  if (empty($_POST['email'])) {
+    $emailErr = 'Email is required';
+  } else {
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+  }
+
+  //validate Feed back
+
+  if (empty($_POST['body'])) {
+    $bodyErr = 'Feedback is required';
+  } else {
+    $body = filter_input(INPUT_POST, 'body', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  }
+
+}
+
+?>
+
+
+
 <img src="./img/logo.png" class="w-25 mb-3" alt="" />
 <h2>Feedback</h2>
 <p class="lead text-center">Leave feedback for anything</p>
-<form action="" class="mt-4 w-75">
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="mt-4 w-75">
   <div class="mb-3">
     <label for="name" class="form-label">Name</label>
-    <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name" />
+    <input type="text" class="form-control <?php echo !$nameErr?: "is-invalid"; ?>" id="name" name="name" placeholder="Enter your name" />
+    <div class="invalid-feedback">
+      <?php echo $nameErr ; ?>
+    </div>
   </div>
   <div class="mb-3">
     <label for="email" class="form-label">Email</label>
